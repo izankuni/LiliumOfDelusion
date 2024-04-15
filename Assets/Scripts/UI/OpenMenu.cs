25bab2c98d1c623c4b6eaec2325a0db0f3c2acf3
izankuni <al404504@uji.es>
2024-04-15 13:14:39 +0200

    Sí

---
+++ Assets/Scripts/UI/OpenMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    [SerializeField] GameObject menu;
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void menuOpen()
+    {
+        menu.SetActive(true);
+    }
+}
